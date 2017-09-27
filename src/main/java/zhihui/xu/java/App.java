9bc09af90d8bbbc3042bf05d188204da190250e5
zhihui.xu <zhihui.xu@qunar.com>
2017-09-27 11:42:58 +0800

    t

---
+++ src/main/java/zhihui/xu/java/App.java
@@ -0,0 +1,13 @@
+package zhihui.xu.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
